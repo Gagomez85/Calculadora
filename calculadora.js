8ea11e465e554eeb4da13bc61880fdda3e11aba1
GOMEZ Gerardo <aez@tgn.com.ar>
2021-04-14 23:37:40 -0300

    second commit

---
+++ calculadora.js
@@ -0,0 +1,9 @@
+let sumar = require('./operaciones/sumar.js');
+//let dividir = require('./operaciones/dividir.js');
+//let multiplicar = require('./operaciones/multiplicar.js');
+//let restar = require('./operaciones/restar.js');
+
+console.log('Prueba de restar')
+//console.log(restar (10,5))
+console.log('Prueba de sumar','\n',sumar(5,5))
+
