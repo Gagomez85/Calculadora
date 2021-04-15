44a4603e515484de63221c3ce9d49527b5997270
GARCIA Ramiro <ramiro.garcia@sagemcom.com>
2021-04-15 00:22:25 -0300

    todo funcionando

--- calculadora.js
+++ calculadora.js
@@ -1,11 +1,11 @@
 let sumar = require('./operaciones/sumar.js');
-//let dividir = require('./operaciones/dividir.js');
-//let multiplicar = require('./operaciones/multiplicar.js');
+let dividir = require('./operaciones/dividir.js');
+let multiplicar = require('./operaciones/multiplicar.js');
 let restar = require('./operaciones/restar.js');
 
 
 
 console.log("La suma es: " + sumar(10,3));
 console.log("La Resta es: " + restar(10,3));
-//console.log("La División es: " + dividir(10,3));
-//console.log("La Multiplicación es: " + multiplicar(10,3));
+console.log("La División es: " + dividir(10,3));
+console.log("La Multiplicación es: " + multiplicar(10,3));
